69b830a66720071218185402bb291363d7bc1d09
syunya ando <wimpsyun@gmail.com>
2024-05-09 18:12:20 +0900

    パスワード入力時、非表示にする

--- quest.sh
+++ quest.sh
@@ -8,14 +8,15 @@ case $choice in
 "Add Password")
 read -p "サービス名を入力してください：" service_name
 read -p "ユーザー名を入力してください：" user_name
-read -p "パスワードを入力してください：" password
+read -s -p  "パスワードを入力してください：" password
 
 echo "
 サービス名:$service_name 
 ユーザー名:$user_name 
 パスワード:$password" >> info.txt
 
-echo "パスワードの追加は成功しました！"
+echo "
+パスワードの追加は成功しました！"
 ;;
 
 "Get Password")
